852497709d8e95e09d405951f5358b5267fe3aae
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-15 16:29:39 +0900

    fix(): outputdto 만들때는 entity참조하지 않도록 한다

--- src/user/dto/get-user.dto.ts
+++ src/user/dto/get-user.dto.ts
@@ -1,8 +1,23 @@
-import { PickType } from '@nestjs/swagger';
-import { User } from '../entities/user.entity';
+import { Payment } from '../../billing/payment/entities/payment.entity';
+import { Subscription } from '../../billing/subscription/entities/subscription.entity';
+import { User, UserRole } from '../entities/user.entity';
 
-export class GetUserOutputDto extends PickType(User, [
-  'email',
-  'payments',
-  'subscriptions',
-] as const) {}
+export class GetUserOutputDto {
+  id: number;
+
+  email: string;
+
+  role: UserRole;
+
+  subscriptions: Subscription[];
+
+  payments: Payment[];
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.email = user.email;
+    this.role = user.role;
+    this.payments = user.payments;
+    this.subscriptions = user.subscriptions;
+  }
+}
